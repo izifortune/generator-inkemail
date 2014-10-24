fa91b6cac33ab01d726e1130e5b58572c2f964db
Fabrizio Fortunato <fortune@cortesconta.net>
2014-10-24 22:50:34 +0200

    added connect to grunt

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -9,6 +9,38 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
 
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: '0.0.0.0',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            '<%= yeoman.app %>'
+          ]
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          base: [
+            '.tmp',
+            'test',
+            '<%= yeoman.app %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= yeoman.dist %>'
+        }
+      }
+    }
   });
 
   grunt.registerTask('serve', [
