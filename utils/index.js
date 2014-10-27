afa2def0b00bf9fcf1090749e23f0aedc286088d
Fabrizio Fortunato <fortune@cortesconta.net>
2014-10-27 09:14:22 +0100

    Utils module ( for now handling template type )

---
+++ utils/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  starters: require('./starters')
+};
---
+++ utils/starters.js
@@ -0,0 +1,33 @@
+module.exports = {
+  templates: [
+    {
+      name: 'Basic',
+      file: 'basic.html'
+    },
+    {
+      name: '[T] Tabs',
+      user: 'diegonetto',
+      repo: 'ionic-starter-tabs'
+    },
+    {
+      name: '[T] Side Menu',
+      user: 'diegonetto',
+      repo: 'ionic-starter-sidemenu'
+    },
+    {
+      name: '[T] Maps',
+      user: 'diegonetto',
+      repo: 'ionic-starter-maps'
+    },
+    {
+      name: '[T] User Intro',
+      user: 'diegonetto',
+      repo: 'ionic-user-intro'
+    },
+    {
+      name: '[A] Pets',
+      user: 'diegonetto',
+      repo: 'ionic-pets-seed'
+    }
+  ]
+};
